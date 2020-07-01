4c89b5f3b28f9baed0ce13ca5c214c3f3671a012
Ytunzhang <60333692+Ytunzhang@users.noreply.github.com>
2020-07-01 17:50:47 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,151 @@
 //返回顶部插件
 $('.to-top').toTop();
-//广告卷动插件
-$(function(){
-	jQuery("#slider-3 .slider").slide({mainCell:".bd ul",titCell:".hd li",trigger:"click",effect:"leftLoop",autoPlay:true,delayTime:700,interTime:7000,pnLoop:false,titOnClassName:"active"})});
+//禁止页面互动插件
+document.oncontextmenu = function(event) { 
+ 
+    if (window.event) { 
+ 
+        event = window.event; 
+ 
+    } 
+ 
+    try { 
+ 
+        var the = event.srcElement; 
+ 
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) { 
+ 
+            return false; 
+ 
+        } 
+ 
+        return true; 
+ 
+    } catch (e) { 
+ 
+        return false; 
+ 
+    } 
+ 
+} 
+ 
+   
+ 
+//屏蔽粘贴 
+document.onpaste = function(event) { 
+    if (window.event) { 
+ 
+        event = window.event; 
+ 
+    } 
+ 
+    try { 
+ 
+        var the = event.srcElement; 
+ 
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) { 
+ 
+            return false; 
+ 
+        } 
+ 
+        return true; 
+ 
+    } catch (e) { 
+ 
+        return false; 
+ 
+    } 
+ 
+} 
+ 
+   
+ 
+//屏蔽复制 
+ 
+document.oncopy = function(event) { 
+ 
+    if (window.event) { 
+ 
+        event = window.event; 
+ 
+    } 
+ 
+    try { 
+ 
+        var the = event.srcElement; 
+ 
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) { 
+ 
+            return false; 
+ 
+        } 
+ 
+        return true; 
+ 
+    } catch (e) { 
+ 
+        return false; 
+ 
+    } 
+ 
+} 
+ 
+   
+ 
+//屏蔽剪切 
+ 
+document.oncut = function(event) { 
+ 
+    if (window.event) { 
+        event = window.event; 
+ 
+    } 
+ 
+    try { 
+ 
+        var the = event.srcElement; 
+ 
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) { 
+ 
+            return false; 
+ 
+        } 
+ 
+        return true; 
+ 
+    } catch (e) { 
+ 
+        return false; 
+ 
+    } 
+ 
+} //屏蔽选中 
+ 
+document.onselectstart = function(event) { 
+ 
+    if (window.event) { 
+ 
+        event = window.event; 
+ 
+    } 
+ 
+    try { 
+ 
+        var the = event.srcElement; 
+ 
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) { 
+ 
+            return false; 
+ 
+        } 
+ 
+        return true; 
+ 
+    } catch (e) { 
+ 
+        return false; 
+ 
+    } 
+ 
+}
