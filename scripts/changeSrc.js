36bf5ac98ad317bcc29f1978df4198cf26b23390
Ytunzhang <60333692+Ytunzhang@users.noreply.github.com>
2020-07-08 15:47:31 +0800

    Add files via upload

---
+++ scripts/changeSrc.js
@@ -0,0 +1,69 @@
+/*13年1周年演唱会OK*/
+function changeSrc_01_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=1018536&bvid=BV1es411Z7C3&cid=1473534&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_01_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=1018536&bvid=BV1es411Z7C3&cid=1473534&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_01_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=1018536&bvid=BV1es411Z7C3&cid=1473534&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_01_04(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=1018536&bvid=BV1es411Z7C3&cid=1473534&page=4&as_wide=1&high_quality=1&danmaku=1";}
+
+/*14年2周年演唱会OK*/
+function changeSrc_02_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2465672&bvid=BV1Qs411U7gU&cid=3860811&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_02_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2465672&bvid=BV1Qs411U7gU&cid=3860811&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_02_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2465672&bvid=BV1Qs411U7gU&cid=3860811&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_02_04(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2465672&bvid=BV1Qs411U7gU&cid=3860811&page=4&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_02_making(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2574255&bvid=BV1Ls411S7db&cid=4021177&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+/*15年3周年演唱会OK*/
+function changeSrc_03_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2050864&bvid=BV1rx411A7dT&cid=4301100&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_03_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2050864&bvid=BV1rx411A7dT&cid=4301100&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_03_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=2050864&bvid=BV1rx411A7dT&cid=4301100&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_03_making(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=5229770&bvid=BV1fs411v7ao&cid=8499495&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+/*X'mas Show 2015*/
+function changeSrc_XmasShow_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=3435654&bvid=BV1zs411972X&cid=5450548&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_XmasShow_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=3435654&bvid=BV1zs411972X&cid=5450548&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_XmasShow_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=3435654&bvid=BV1zs411972X&cid=5450548&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_XmasShow_04(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=3435654&bvid=BV1zs411972X&cid=5450548&page=4&as_wide=1&high_quality=1&danmaku=1";}
+
+/*16年4周年演唱会 过长直接跳转
+function changeSrc_04_01_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_01_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_01_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_02_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=4&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_02_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=5&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_02_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=6&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_03_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=7&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_03_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=8&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_04_03_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11722455&bvid=BV1Ux41167Zs&cid=19364471&page=9&as_wide=1&high_quality=1&danmaku=1";}
+*/
+function changeSrc_04_making(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=11721103&bvid=BV1Sx41167G9&cid=19362446&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+/*17年5周年桥本奈奈未毕业演唱会 后两天直接跳转*/
+function changeSrc_05_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=21351430&bvid=BV1cW411u7Gk&cid=35110788&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_02_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=21351430&bvid=BV1cW411u7Gk&cid=35110788&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_05_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=21351430&bvid=BV1cW411u7Gk&cid=35110788&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_05_making(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=21394467&bvid=BV1LW411u7VE&cid=35193719&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+/*17年首次东京巨蛋*/
+function changeSrc_TokyoDome_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=26650816&bvid=BV1ns411H7sJ&cid=45839911&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_TokyoDome_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=26650816&bvid=BV1ns411H7sJ&cid=45839911&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_TokyoDome_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=26650816&bvid=BV1ns411H7sJ&cid=45839911&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_TokyoDome_04(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=26650816&bvid=BV1ns411H7sJ&cid=45839911&page=4&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_TokyoDome_making(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=26612038&bvid=BV1gs411H7gk&cid=45766807&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+/*18年6周年演唱会*/
+function changeSrc_06_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=58039154&bvid=BV1C4411c76i&cid=103060958&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_06_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=58039154&bvid=BV1C4411c76i&cid=103060958&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_06_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=58039154&bvid=BV1C4411c76i&cid=103060958&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_06_sp(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=57896564&bvid=BV184411A7mW&cid=102774274&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+/*19年7周年西野七濑毕业演唱会*/
+function changeSrc_07_04_01(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=88954155&bvid=BV157411g7Wv&cid=151934897&page=1&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_07_04_02(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=88954155&bvid=BV157411g7Wv&cid=151934897&page=2&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_07_04_03(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=88954155&bvid=BV157411g7Wv&cid=151934897&page=3&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_07_04_04(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=88954155&bvid=BV157411g7Wv&cid=151934897&page=4&as_wide=1&high_quality=1&danmaku=1";}
+function changeSrc_07_sp(){document.getElementById("bv").src="https://player.bilibili.com/player.html?aid=86819906&bvid=BV1m7411n7yX&cid=148408157&page=1&as_wide=1&high_quality=1&danmaku=1";}
+
+
+
+
+
+
